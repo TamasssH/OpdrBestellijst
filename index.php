ea9de2213d6ae3c0ebcfeebba9d825dbbc7cc214
TamasH <unconfigured@null.spigotmc.org>
2023-02-01 15:56:40 +0100

    Index.php + basic pizza bestelijst die niks doet nog

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzeria Pizzer di sog</title>
+</head>
+<body>
+
+    <form action="" method="post">
+
+        <label for="fname">Voornaam: </label>
+        <input type="text" id="fname" name="fname">
+
+        <label for="lname">Achternaam: </label>
+        <input type="text" id="lname" name="lname">
+
+        <label for="adres">Adres: </label>
+        <input type="text" id="adres" name="adres">
+
+        <label for="postcode">Postcode: </label>
+        <input type="text" id="postcode" name="postcode">
+
+        <label for="plaats">Plaats: </label>
+        <input type="text" id="plaats" name="plaats">
+
+        <label for="besteldatum">Besteldatum: </label>
+        <input type="text" id="besteldatum" name="besteldatum">
+
+        <label for="bezorgKeuze"></label>
+        <select>
+            <option value="bezorgen">Laten bezorgen</option>
+            <option value="afhalen">Afhalen</option>
+        </select>
+
+        <input type="submit">
+
+    </form>
+
+    <?php 
+    
+    
+    
+    ?>
+</body>
+
+</html>
