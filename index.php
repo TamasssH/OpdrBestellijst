99c2291644a6e3f402d6ee207fb37754c059f548
TamasH <unconfigured@null.spigotmc.org>
2023-02-01 16:12:26 +0100

    placeholders + line breaks

--- index.php
+++ index.php
@@ -11,30 +11,31 @@
     <form action="" method="post">
 
         <label for="fname">Voornaam: </label>
-        <input type="text" id="fname" name="fname">
+        <input type="text" id="fname" name="fname" placeholder="voer uw naam in."><br />
 
         <label for="lname">Achternaam: </label>
-        <input type="text" id="lname" name="lname">
+        <input type="text" id="lname" name="lname" placeholder="Voer uw achternaam in."><br />
 
-        <label for="adres">Adres: </label>
-        <input type="text" id="adres" name="adres">
+        <label for="adres" >Adres: </label>
+        <input type="text" id="adres" name="adres" placeholder="Voer uw adres in."><br />
 
         <label for="postcode">Postcode: </label>
-        <input type="text" id="postcode" name="postcode">
+        <input type="text" id="postcode" name="postcode" placeholder="Voer uw postcode in."><br />
 
         <label for="plaats">Plaats: </label>
-        <input type="text" id="plaats" name="plaats">
+        <input type="text" id="plaats" name="plaats" placeholder="Voer uw plaats naam in."><br />
 
         <label for="besteldatum">Besteldatum: </label>
-        <input type="text" id="besteldatum" name="besteldatum">
+        <input type="text" id="besteldatum" name="besteldatum" placeholder="Voer uw besteldatum in."><br />
 
         <label for="bezorgKeuze"></label>
         <select>
             <option value="bezorgen">Laten bezorgen</option>
             <option value="afhalen">Afhalen</option>
+            <br />
         </select>
 
-        <input type="submit">
+       <input type="submit">
 
     </form>
 
