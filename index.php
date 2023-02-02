c49020bb817b44152bf981dbaad1acc9024e9439
TamasH <unconfigured@null.spigotmc.org>
2023-02-02 09:05:03 +0100

    alles in divs geplaatst + bestellijst

--- index.php
+++ index.php
@@ -8,40 +8,50 @@
 </head>
 <body>
 
-    <form action="" method="post">
+    <div class="bestelLijst">
 
-        <label for="fname">Voornaam: </label>
-        <input type="text" id="fname" name="fname" placeholder="voer uw naam in."><br />
+        <h2>Pizza Margherita <span>12,50 euro</span></h2>
+        <h2>Pizza Funghi <span>12,50 euro</span></h2>
+        <h2>Pizza Marina <span>13,95 euro</span></h2>
+        <h2>Pizza Hawaii <span>11,50 euro</span></h2>
+        <h2>Pizza Quattro Formaggi <span>14,50 euro</span></h2>
 
-        <label for="lname">Achternaam: </label>
-        <input type="text" id="lname" name="lname" placeholder="Voer uw achternaam in."><br />
+    </div>
 
-        <label for="adres" >Adres: </label>
-        <input type="text" id="adres" name="adres" placeholder="Voer uw adres in."><br />
+    <div class="Form">
 
-        <label for="postcode">Postcode: </label>
-        <input type="text" id="postcode" name="postcode" placeholder="Voer uw postcode in."><br />
+        <form action="" method="post">
 
-        <label for="plaats">Plaats: </label>
-        <input type="text" id="plaats" name="plaats" placeholder="Voer uw plaats naam in."><br />
+            <label for="fname">Voornaam: </label>
+            <input type="text" id="fname" name="fname" placeholder="voer uw naam in."><br />
 
-        <label for="besteldatum">Besteldatum: </label>
-        <input type="text" id="besteldatum" name="besteldatum" placeholder="Voer uw besteldatum in."><br />
+            <label for="lname">Achternaam: </label>
+            <input type="text" id="lname" name="lname" placeholder="Voer uw achternaam in."><br />
 
-        <label for="bezorgKeuze"></label>
-        <select>
-            <option value="bezorgen">Laten bezorgen</option>
-            <option value="afhalen">Afhalen</option>
-            <br />
-        </select>
+            <label for="adres" >Adres: </label>
+            <input type="text" id="adres" name="adres" placeholder="Voer uw adres in."><br />
 
-       <input type="submit">
+            <label for="postcode">Postcode: </label>
+            <input type="text" id="postcode" name="postcode" placeholder="Voer uw postcode in."><br />
 
-    </form>
+            <label for="plaats">Plaats: </label>
+            <input type="text" id="plaats" name="plaats" placeholder="Voer uw plaats naam in."><br />
 
-    <?php 
-    
-    
+            <label for="besteldatum">Besteldatum: </label>
+            <input type="text" id="besteldatum" name="besteldatum" placeholder="Voer uw besteldatum in."><br />
+
+            <label for="bezorgKeuze"></label>
+            <select>
+                <option value="bezorgen">Laten bezorgen</option>
+                <option value="afhalen">Afhalen</option>
+                <br />
+            </select>
+
+            <input type="submit">
+</form>
+    </div>
+
+    <?php
     
     ?>
 </body>
