cad69795b9a75f1d09695a7cf36805a89d7a937f
TamasH <unconfigured@null.spigotmc.org>
2023-02-08 12:55:36 +0100

    test verwijderd

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<?php   
-    if(isset($_POST["submit"])){
 
-        $fname = $_POST["fname"];
-        $lname = $_POST["lname"];
-        $adres =  $_POST["adres"];
-        $postcode =  $_POST["postcode"];
-        $plaats =  $_POST["plaats"];
-        $besteldatum =  $_POST["besteldatum"];
-        $bezorg =  $_POST["bezorg"];
-
-    }
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -155,9 +143,22 @@
                     </select>    
             </div>
 
-                <input type="submit" value="Bestelling afronden.">
+                <input type="submit" value="bestellen" name="order">
 
+                <?php   
+                    if(isset($_POST["order"])){
 
+                        $fname = $_POST["fname"];
+                        $lname = $_POST["lname"];
+                        $adres =  $_POST["adres"];
+                        $postcode =  $_POST["postcode"];
+                        $plaats =  $_POST["plaats"];
+                        $besteldatum =  $_POST["besteldatum"];
+                        $bezorg =  $_POST["bezorg"];
+
+                    echo "hello please help me i am forced to do php.";
+                    }
+                ?>
         </form>
         
     </div>
